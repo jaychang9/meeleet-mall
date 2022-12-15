a710121f5a409cc30a609d3de7259b92fe422bd6
jaychang <jaychang1987@gmail.com>
2022-12-15 14:04:48 +0800

    fix: meeleet-gateway.yaml mall-admin-web add README.md

--- mall-web-admin/src/main/java/com/meeleet/mall/admin/controller/SysUserController.java
+++ mall-web-admin/src/main/java/com/meeleet/mall/admin/controller/SysUserController.java
@@ -3,6 +3,7 @@ package com.meeleet.mall.admin.controller;
 import com.meeleet.cloud.common.result.R;
 import com.meeleet.cloud.sys.pojo.dto.SysUserDTO;
 import com.meeleet.cloud.sys.rpc.ISysUserRpcService;
+import io.swagger.v3.oas.annotations.Operation;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -18,11 +19,13 @@ public class SysUserController {
     @DubboReference
     private ISysUserRpcService sysUserRpcService;
 
+    @Operation(description = "系统用户信息")
     @GetMapping("/{id:\\d+}")
     public R<SysUserDTO> list(@PathVariable("id") Long id) {
         return R.success(sysUserRpcService.findById(id));
     }
 
+    @Operation(description = "系统用户列表")
     @GetMapping("/list")
     public R<List<SysUserDTO>> list() {
         return R.success(sysUserRpcService.list());
