5977f8be64fda9d7b11a5031eb7902e36298e984
jaychang <jaychang1987@gmail.com>
2022-12-15 14:36:34 +0800

    feat: 查当前登录用户信息

--- mall-web-admin/src/main/java/com/meeleet/mall/admin/controller/SysUserController.java
+++ mall-web-admin/src/main/java/com/meeleet/mall/admin/controller/SysUserController.java
@@ -1,6 +1,7 @@
 package com.meeleet.mall.admin.controller;
 
 import com.meeleet.cloud.common.result.R;
+import com.meeleet.cloud.common.security.util.SysUserUtils;
 import com.meeleet.cloud.sys.pojo.dto.SysUserDTO;
 import com.meeleet.cloud.sys.rpc.ISysUserRpcService;
 import io.swagger.v3.oas.annotations.Operation;
@@ -19,10 +20,18 @@ public class SysUserController {
     @DubboReference
     private ISysUserRpcService sysUserRpcService;
 
-    @Operation(description = "系统用户信息")
+    @Operation(description = "指定ID的系统用户信息")
     @GetMapping("/{id:\\d+}")
-    public R<SysUserDTO> list(@PathVariable("id") Long id) {
+    public R<SysUserDTO> info(@PathVariable("id") Long id) {
         return R.success(sysUserRpcService.findById(id));
+    }
+
+
+    @Operation(description = "当前登录者的系统用户信息")
+    @GetMapping("/me")
+    public R<SysUserDTO> me() {
+        Long userId = SysUserUtils.getUserId();
+        return R.success(sysUserRpcService.findById(userId));
     }
 
     @Operation(description = "系统用户列表")
