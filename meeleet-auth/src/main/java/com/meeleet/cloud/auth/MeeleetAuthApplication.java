ef43ccc399645b53b4c59ee14d029868debcaba5
jaychang <jaychang1987@gmail.com>
2022-12-15 13:56:58 +0800

    feat: meeleet-auth meeleet-common-web->meeleet-web-spring-boot-starter

--- meeleet-auth/src/main/java/com/meeleet/cloud/auth/MeeleetAuthApplication.java
+++ meeleet-auth/src/main/java/com/meeleet/cloud/auth/MeeleetAuthApplication.java
@@ -1,8 +1,6 @@
 package com.meeleet.cloud.auth;
 
-import com.meeleet.cloud.common.i18n.UnifiedMessageSource;
 import com.meeleet.cloud.common.redis.RedisConfig;
-import com.meeleet.cloud.common.web.exception.GlobalExceptionHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
@@ -10,7 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Import;
 
 // TODO 这里可以用借鉴使用自动加载
-@Import({GlobalExceptionHandler.class, RedisConfig.class, UnifiedMessageSource.class})
+@Import({RedisConfig.class})
 @EnableDiscoveryClient
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class MeeleetAuthApplication {
