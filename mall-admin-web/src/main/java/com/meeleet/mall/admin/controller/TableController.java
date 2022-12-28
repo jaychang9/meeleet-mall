8829ac052a27e25ff49e9638f104af5f2e3fce74
jaychang <jason123>
2022-12-28 14:17:46 +0800

    增加分页方法

--- mall-admin-web/src/main/java/com/meeleet/mall/admin/controller/TableController.java
+++ mall-admin-web/src/main/java/com/meeleet/mall/admin/controller/TableController.java
@@ -3,6 +3,7 @@ package com.meeleet.mall.admin.controller;
 import cn.hutool.core.date.DatePattern;
 import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.RandomUtil;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.meeleet.cloud.common.result.R;
 import io.swagger.v3.oas.annotations.Operation;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +11,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.*;
+import java.util.stream.Collectors;
 
 
+/**
+ * @author jaychang
+ */
 @RestController
 @RequestMapping("/table")
 public class TableController {
@@ -22,7 +27,7 @@ public class TableController {
     public R<List<Map<String, Object>>> list() {
         String[] statusArray = new String[]{"published", "draft", "deleted"};
         List<Map<String, Object>> tableList = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 200; i++) {
             Map<String, Object> t = new HashMap<>();
             t.put("id", i);
             t.put("title", "title" + i);
@@ -33,5 +38,27 @@ public class TableController {
             tableList.add(t);
         }
         return R.success(tableList);
+    }
+
+    @Operation(description = "Table分页列表(仅用于vue admin template调试用)")
+    @GetMapping("/page")
+    public R<Page<Map<String, Object>>> page(int current, int size) {
+        String[] statusArray = new String[]{"published", "draft", "deleted"};
+        List<Map<String, Object>> tableList = new ArrayList<>();
+        int TOTAL = 2500;
+        for (int i = 0; i < TOTAL; i++) {
+            Map<String, Object> t = new HashMap<>();
+            t.put("id", i);
+            t.put("title", "title" + i);
+            t.put("author", "author" + i);
+            t.put("pageviews", i + 100);
+            t.put("status", statusArray[RandomUtil.randomInt(0, 3)]);
+            t.put("displayTime", DateUtil.format(new Date(), DatePattern.NORM_DATETIME_FORMAT));
+            tableList.add(t);
+        }
+        Page<Map<String, Object>> page = new Page<>();
+        page.setCurrent(current).setSize(size).setTotal(TOTAL);
+        page.setRecords(tableList.stream().skip((current - 1) * size).limit(size).collect(Collectors.toList()));
+        return R.success(page);
     }
 }
